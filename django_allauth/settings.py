b3276b917d67c9e53f7197c95bab109758b25877
Thomas Daniel <thomas@thomas-daniel.com>
2018-08-21 15:35:56 -0700

    added google

--- django_allauth/settings.py
+++ django_allauth/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
 
     'authenticate',
