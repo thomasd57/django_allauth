16d65141da7a9c4eb1caf5f666f645b84491c5c8
Thomas Daniel <thomas@thomas-daniel.com>
2018-08-21 14:32:35 -0700

    working github, looping over social providers

--- django_allauth/settings.py
+++ django_allauth/settings.py
@@ -147,7 +147,7 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 
 # Make email verification mandatory to avoid junk email accounts
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+SOCIAL_ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Eliminate need to provide username, as it's a very old practice
 ACCOUNT_USERNAME_REQUIRED = False
