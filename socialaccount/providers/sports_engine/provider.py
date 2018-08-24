816c1e9962c24050bc420c245057232210abe0cc
Thomas Daniel <thomas@thomas-daniel.com>
2018-08-23 21:56:28 -0700

    added loggers

--- socialaccount/providers/sports_engine/provider.py
+++ socialaccount/providers/sports_engine/provider.py
@@ -1,17 +1,25 @@
+import logging
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 
 class SportsEngineAccount(ProviderAccount):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+
     def get_profile_url(self):
-        return self.account.extra_data.get('html_url')
+        profile_url = self.account.extra_data.get('html_url')
+        self.logger.debug('profile_url=' + str(profile_url))
+        return profile_url
 
     def get_avatar_url(self):
-        return self.account.extra_data.get('avatar_url')
+        avatar_url =  self.account.extra_data.get('avatar_url')
+        self.logger.debug('avatar_url=' + str(avatar_url))
 
     def to_str(self):
         dflt = super().to_str()
-        return next(
+        s = next(
             value
             for value in (
                 self.account.extra_data.get('name', None),
@@ -20,20 +28,31 @@ class SportsEngineAccount(ProviderAccount):
             )
             if value is not None
         )
+        self.logger.debug('to_str=' + str(s))
+        return s
+
 class SportsEngineProvider(OAuth2Provider):
     id = 'sports_engine'
     name = 'SportsEngine'
     account_class = SportsEngineAccount
     package = 'socialaccount.providers.sports_engine'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+
     def extract_uid(self, data):
-        data = data['identity']
-        return str(data['id'])
+        data = str(data['identity']['id'])
+        self.logger.debug('extract_uid=' + data)
+        return data
 
     def extract_common_fields(self, data):
-        return dict(email=data.get('email'),
-                    username=data.get('login'),
-                    name=data.get('name'))
+        fields = dict(email=data.get('email'),
+                      username=data.get('login'),
+                      name=data.get('name'))
+        self.logger.debug('extract_common_fields,email={}, username={}, name={}'.format(
+            fields['email'], fields['username'], fields['name']))
+        return fields
 
 # providers_classes = [SportsEngineProvider]
 providers.registry.register(SportsEngineProvider)
--- socialaccount/providers/sports_engine/views.py
+++ socialaccount/providers/sports_engine/views.py
@@ -1,3 +1,5 @@
+import logging
+import pprint
 import requests
 from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
                                                           OAuth2LoginView,
@@ -10,12 +12,18 @@ class SportsEngineOAuth2Adapter(OAuth2Adapter):
     authorize_url = 'https://user.sportngin.com/oauth/authorize'
     profile_url = ''
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+
     def complete_login(self, request, app, token, **kwargs):
         headers = {'Authorization': 'Bearer {0}'.format(token.token)}
         resp = requests.get(self.profile_url, headers=headers)
         extra_data = resp.json()
-        return self.get_provider().sociallogin_from_response(request,
+        data = self.get_provider().sociallogin_from_response(request,
                                                              extra_data)
+        self.logger.debug('complete_login=' + pprint.pformat(data))
+        return data
 
 oauth2_login = OAuth2LoginView.adapter_view(SportsEngineOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(SportsEngineOAuth2Adapter)
